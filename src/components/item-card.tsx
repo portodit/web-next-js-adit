169d57baa867a6bf6d445243eda7946e542ee43e
PORTODIT <97856960+portodit@users.noreply.github.com>
2024-05-30 18:23:53 +0700

    Update item-card.tsx

--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -1,32 +1,15 @@
 import React from "react";
 
-interface Pokemon {
-  id: number;
+interface DogProps {
   name: string;
-  genus: string;
-  description: string;
   imageUrl: string;
-  types: string[];
 }
 
-interface Props {
-  pokemon: Pokemon;
-}
-
-const ItemCard: React.FC<Props> = ({ pokemon }) => {
+const ItemCard: React.FC<DogProps> = ({ name, imageUrl }) => {
   return (
     <div className="card">
-      <img className="card-img" src={pokemon.imageUrl} alt={pokemon.name} />
-      <h2>{pokemon.name}</h2>
-      <p>{pokemon.genus}</p>
-      <p>{pokemon.description}</p>
-      <div className="card-types">
-        {pokemon.types.map((type, index) => (
-          <span key={index} className="type-label">
-            {type}
-          </span>
-        ))}
-      </div>
+      <img src={imageUrl} alt={name} width="400" height="400" />
+      <h2>{name}</h2>
     </div>
   );
 };
